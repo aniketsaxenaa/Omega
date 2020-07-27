3fc7ea1845e8adc4290d69c5e064f5465ab7c57b
aniketsaxenaa <68874569+aniketsaxenaa@users.noreply.github.com>
2020-07-28 02:09:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyDfZiaAeaZ1s6JcTdZgmgm8BJC4Nh01pG8",
+  authDomain: "insta-51325.firebaseapp.com",
+  databaseURL: "https://insta-51325.firebaseio.com",
+  projectId: "insta-51325",
+  storageBucket: "insta-51325.appspot.com",
+  messagingSenderId: "733443121702",
+  appId: "1:733443121702:web:34f42e177c037cadf8fce7",
+  measurementId: "G-1FFZNKD49K"
 };
 
      
